fa4b5becc119fcfe3da67e954b9602839b50c7fd
AgrawalAnishka <anishkaagrawalfake@gmail.com>
2026-08-22 19:17:21 +0530

    Time: 3 ms (88.97%) | Memory: 23.9 MB (59.62%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& nums) {
-        vector<vector<int>>  ans ; 
+        vector<vector<int>> ans  ; 
         sort(nums.begin() , nums.end());
-        if(nums.size()==0) return {};
         vector<int> temp = nums[0];
-        vector<vector<int>> res ; 
-        for(int i = 0; i <nums.size() ; i++){
-                if(nums[i][0] > temp[1]){
-                    ans.push_back(temp);
-                    temp = nums[i];
-                }else{
-                    temp[1]  = max(temp[1] ,  nums[i][1]);
-                }
-            
+
+        for(int i  =1 ;i < nums.size() ; i++){
+            if(temp[1] >=  nums[i][0]){
+                temp[1] = max(temp[1] , nums[i][1]);
+            }else{
+                ans.push_back(temp);
+                temp = nums[i];
+            }
         }
         ans.push_back(temp);
-
         return ans ;
     }
 };
